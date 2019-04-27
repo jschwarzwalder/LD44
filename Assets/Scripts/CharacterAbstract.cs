b3651a5b6c74f4540432dfe9726d6ea585f93306
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-04-27 15:18:26 -0700

    Create basic enemey spawn with Character Script

---
+++ Assets/Scripts/CharacterAbstract.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Void;
+
+public class CharacterAbstract : MonoBehaviour
+{
+    [SerializeField] float Health;
+    [SerializeField] int Damage;
+    [SerializeField] float timeBetweenCast;
+    [SerializeField] float Range;
+
+
+    public void Shoot()
+    {
+
+
+
+    }
+}
---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    private int enemyIndex;
+    private float time = 0.0f;
+
+
+    [SerializeField] Enemy[] toSpawn;
+    [SerializeField] float spawnDelay;
+    [SerializeField] float groundLevel;
+    [SerializeField] float max;
+    [SerializeField] float min;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        enemyIndex = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >= spawnDelay)
+        {
+            time = 0.0f;
+
+            Spawn();
+
+        }
+
+    }
+
+    private void Spawn()
+    {
+        Debug.Log("Spawn");
+        GameObject enemy = getNextEnemy();
+        Debug.Log("Enemy: " + enemy);
+        if (enemy != null)
+        {
+            enemy.transform.localScale = new Vector3(1, 1, 1);
+            GameObject newSpawn = GameObject.Instantiate(enemy.gameObject);
+            Vector3 enemyPosition = this.transform.position;
+            enemyPosition.x += Random.Range(min, max);
+            enemyPosition.y = groundLevel;
+            newSpawn.transform.position = enemyPosition;
+
+        }
+
+
+    }
+
+    protected GameObject getNextEnemy()
+    {
+        if (enemyIndex < toSpawn.Length)
+        {
+            GameObject currentEnemy = toSpawn[enemyIndex];
+            enemyIndex += 1;
+            return currentEnemy;
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+
+
+}
+
+
+
+
