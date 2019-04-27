befb163abc680fd0189bd9ba00ff4c71d93e962a
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-04-27 15:20:17 -0700

    Adding LineRender to Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : CharacterAbstract 
+
+{
+
+    private LineRenderer laserLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
