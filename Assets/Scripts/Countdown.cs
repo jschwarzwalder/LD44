cb0d13d1d51e62f02b1690b98a0a7da31539fba5
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-04-28 10:42:41 -0700

    Creating Interactive Canvas

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+
+    public float time;
+    private Text text;
+    [SerializeField] GameObject levelSelect;
+    [SerializeField] GameObject TimerDisplay;
+    [SerializeField] AudioSource Music;
+    private float musicVolume;
+
+    public float getTime() { return time; }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+        if (TimerDisplay)
+        {
+            TimerDisplay.SetActive(true);
+        }
+        if (levelSelect)
+        {
+            levelSelect.SetActive(false);
+        }
+        if (Music)
+        {
+            musicVolume = Music.volume;
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (time > 0.0000f)
+        {
+            time -= Time.deltaTime;
+            if (text)
+            {
+                text.text = "" + time.ToString("F");
+            }
+        }
+        else
+        {
+            if (TimerDisplay)
+            {
+                TimerDisplay.SetActive(false);
+                levelSelect.SetActive(true);
+
+            }
+            Music.volume = musicVolume / 4;
+            //Debug.Log(Music.volume);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    private int Level;
+
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void NextScene()
+    {
+        int level = Level + 1;
+        SceneManager.LoadScene(level);
+    }
+    public void Start()
+    {
+        Level = SceneManager.GetActiveScene().buildIndex;
+    }
+
+}
