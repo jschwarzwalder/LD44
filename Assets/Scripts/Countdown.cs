354f044fcf01044d2c7e078bdf25e5b3feacf1cf
jschwarzwalder <jamischwarzwader@gmail.com>
2019-04-28 17:43:32 -0700

    Level Music now persists.

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -65,4 +65,8 @@ public class Countdown : MonoBehaviour
             //Debug.Log(Music.volume);
         }
     }
+
+    private void OnDestroy () {
+        Music.volume = musicVolume;
+    }
 }
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour
+{
+    public static Singleton<T> Instance { get; protected set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance != null) {
+            Destroy(gameObject);
+        }
+        else {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
