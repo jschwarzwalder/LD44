2a8b8931343f1e19681a77635ce1bef92633ac50
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-05-21 23:37:25 -0700

    adding level reset on r press

---
+++ Assets/Scripts/GameReset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameReset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.DontDestroyOnLoad(gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+    }
+}
